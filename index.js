a6961d0b4f6381e1b673bafb4ff6a33e00ee1d56
Edoardo Lenzi <edoardolenzi9@gmail.com>
2019-05-31 18:31:30 +0200

    [WIP] mesh composition

--- index.js
+++ index.js
@@ -20,21 +20,63 @@ function Init() {
 	material.color.setHex( 0xf0f0f0 );
 	mesh = new THREE.Mesh( geometry, material );
 	//scene.add( mesh );
-	LoadGLTF('assets/models/inner_structure/inner_structure.glb', 100);	
-	LoadGLTF( 'assets/models/acc_ring/acc_ring.glb', 50);	
-	LoadGLTF( 'assets/models/acc_coil/acc_coil.glb', 100);	
-	LoadGLTF( 'assets/models/chamber/chamber.glb', 100);	
-	LoadGLTF( 'assets/models/chamber_heatsink/chamber_heatsink.glb', 100);	
-	LoadGLTF( 'assets/models/chamber_rings/chamber_rings.glb', 100);	
-	LoadGLTF( 'assets/models/grid/grid.glb', 100);	
-	LoadGLTF( 'assets/models/inner_copper_rings/inner_copper_rings.glb', 100);	
-	LoadGLTF( 'assets/models/inner_structure/inner_structure.glb', 100);	
+	readTextFile("assets/models/architecture.csv");
 	InitCamera();
 	
 	window.addEventListener( 'resize', OnWindowResize, false );
 	
 	InitRenderer();
 }
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                ParseArchitecture(rawFile.responseText);   
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+
+function ParseArchitecture(csv)
+{
+	csv = csv.split('\n');
+	csv.shift(); 
+	csv.pop();
+	csv.forEach(function(row) {
+  		row = row.split(',');
+		SetupMesh(
+			row[0], 
+			row[1],
+			new THREE.Vector3(row[2], row[3], row[4]),
+			new THREE.Vector3(row[5], row[6], row[7]),
+			new THREE.Vector3(row[8], row[9], row[10]),
+			new THREE.Vector3(row[11], row[12], row[13])	
+		);
+	});
+}
+
+
+function SetupMesh(url, scale, init_pos, init_rotation, final_pos, final_rotation){
+	var loader = new THREE.GLTFLoader();
+	loader.load( url, function( gltf ) {
+		mesh = gltf.scene.children[ 0 ];
+		mesh.scale.set( scale, scale, scale );
+		mesh.position.set( init_pos.x, init_pos.y, init_pos.z );
+		mesh.rotation.set( init_rotation.x* 180/Math.PI, init_rotation.y* 180/Math.PI, init_rotation.z* 180/Math.PI );
+		scene.add( mesh );
+	} );
+}
+
 
 function LoadGLTF(url, scale){
 	var loader = new THREE.GLTFLoader();
